9b42e9f2a2697e0b45e30d2abb5e5d3b69dc3e4d
ZeynepDuzen <zeynepduzen59@gmail.com>
2023-07-20 09:56:32 +0300

    Proje dosyası ekle.

---
+++ IfElse2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfElse2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sehir;
+            sehir = "bursa";
+
+            Console.WriteLine("Lütfen sehir adı giriniz : ");
+            Console.WriteLine();
+            sehir = Console.ReadLine();
+
+            if (sehir == "bursa")
+            {
+                
+                Console.WriteLine("Dogru sehir");
+            }
+            else
+            {
+                Console.WriteLine("Yanlıs sehir");
+            }
+
+            Console.Read();
+        }
+    }
+}
